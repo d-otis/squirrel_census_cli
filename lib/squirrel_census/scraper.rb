7210952e575722cd8b6d5d83454fe51fa71444d9
d-otis <dan@dan-foley.com>
2020-02-27 17:39:16 -0500

    uncomment Spacer include

--- lib/squirrel_census/scraper.rb
+++ lib/squirrel_census/scraper.rb
@@ -7,33 +7,6 @@ class SquirrelCensus::Scraper
 		json = JSON.parse(res)
 	end
 
-	# def make_squirrels
-	# 	get_json.each do |squirrel|
-	# 		new_squirrel = SquirrelCensus::Squirrel.new
-	# 		new_squirrel.unique_squirrel_id = squirrel["unique_squirrel_id"]
-	# 		new_squirrel.hectare = squirrel["hectare"]
-	# 		new_squirrel.shift = squirrel["shift"]
-	# 		new_squirrel.date = SquirrelCensus::Date.find_or_create_by_date(squirrel['date'], new_squirrel)
-	# 		new_squirrel.primary_fur_color = squirrel["primary_fur_color"] if squirrel["primary_fur_color"]
-	# 		new_squirrel.highlight_fur_color = squirrel["highlight_fur_color"] if squirrel["highlight_fur_color"]
-	# 		new_squirrel.combination_of_primary_and = squirrel["combination_of_primary_and"] if squirrel["highlight_fur_color"] && squirrel["primary_fur_color"]
-	# 		new_squirrel.age = squirrel["age"]
-	# 		new_squirrel.running = squirrel["running"]
-	# 		new_squirrel.chasing = squirrel["chasing"]
-	# 		new_squirrel.climbing = squirrel["climbing"]
-	# 		new_squirrel.eating = squirrel["eating"]
-	# 		new_squirrel.foraging = squirrel["foraging"]
-	# 		new_squirrel.kuks = squirrel["kuks"]
-	# 		new_squirrel.quaas = squirrel["quaas"]
-	# 		new_squirrel.moans = squirrel["moans"]
-	# 		new_squirrel.tail_flags = squirrel["tail_flags"]
-	# 		new_squirrel.tail_twitches = squirrel["tail_twitches"]
-	# 		new_squirrel.approaches = squirrel["approaches"]
-	# 		new_squirrel.indifferent = squirrel["indifferent"]
-	# 		new_squirrel.runs_from = squirrel["runs_from"]
-	# 	end
-	# end
-
 	def get_squirrel_aoh
 		get_json.collect do |squirrel|
 			# binding.pry
